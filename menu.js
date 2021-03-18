27062943c9613084fecd380977724c7cb747de91
Davonta <davonta2222@gmail.com>
2021-03-18 00:20:32 -0400

    cleaner

--- menu.js
+++ menu.js
@@ -8,6 +8,10 @@ closeMenu.addEventListener("click" ,close);
 
 function show() {
     mainMenu.style.display = 'flex';
-    mainMenu.style.top = 'flex';
+    mainMenu.style.top = '0';
+
+}
+function close(){
+    mainMenu.style.top = '-100%';
 
 }
