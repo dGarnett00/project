0dbc8e3f778d5d07e176831c8c006b2a2efc1eae
Davonta <davonta2222@gmail.com>
2021-03-18 13:21:23 -0400

    menu transition

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector
+(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+    navbar.classList.toggle("change");
+});
